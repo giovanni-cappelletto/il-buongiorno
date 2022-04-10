3aabbfcbb1ac33b241e273804b55062649531dd1
Istituto Don Bosco <100870601+donboscopadova@users.noreply.github.com>
2022-04-10 12:13:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // Variables 
-const month = document.querySelector('#month')
-const day = document.querySelector('#day')
-const pageNumber = document.querySelector('#number')
-const args = document.querySelector('#args')
-const periodical = document.querySelector('.periodical')
-const leftArrow = document.querySelector('#left-arrow')
-const rightArrow = document.querySelector('#right-arrow')
-const dots = document.querySelectorAll('.dot')
-let counter = 0, mese
+// const month = document.querySelector('#month')
+// const day = document.querySelector('#day')
+// const pageNumber = document.querySelector('#number')
+// const args = document.querySelector('#args')
+// const periodical = document.querySelector('.periodical')
+// const leftArrow = document.querySelector('#left-arrow')
+// const rightArrow = document.querySelector('#right-arrow')
+// const dots = document.querySelectorAll('.dot')
+// let counter = 0, mese
 
 // Database
-const database = [
-    {
-        "month": "Marzo",
-        "day": "10 Marzo 2022", 
-        "pages": "20", 
-        "arguments": "guerra in Ucraina, interviste, Olimpiadi e molto altro!",
-        "path": "/pdf/giornalino.pdf"
-    },
+// const database = [
+    // {
+    //     "month": "Marzo",
+    //     "day": "10 Marzo 2022", 
+    //     "pages": "20", 
+    //     "arguments": "guerra in Ucraina, interviste, Olimpiadi e molto altro!",
+    //    "path": "/pdf/giornalino.pdf"
+    // },
     // {
     //     "month": "Aprile",
     //     "day": "10 Aprile 2022", 
@@ -35,21 +35,21 @@ const database = [
 ]
 
 // Display datas
-const start = (n) => {
-    month.textContent = database[n].month
-    day.textContent = database[n].day
-    pageNumber.textContent = database[n].pages
-    args.textContent = database[n].arguments
-    periodical.setAttribute('href', database[n].path) 
+// const start = (n) => {
+//     month.textContent = database[n].month
+//     day.textContent = database[n].day
+//     pageNumber.textContent = database[n].pages
+//     args.textContent = database[n].arguments
+//     periodical.setAttribute('href', database[n].path) 
+// 
+//     for (let i = 0; i < dots.length; i++) {
+//         dots[i].classList.remove('active')
+//     }
+// 
+//     dots[n].classList.add('active')
+// }
 
-    for (let i = 0; i < dots.length; i++) {
-        dots[i].classList.remove('active')
-    }
-
-    dots[n].classList.add('active')
-}
-
-window.addEventListener('load', start(counter))
+// window.addEventListener('load', start(counter))
 
 // leftArrow.addEventListener('click', () => {
 //     counter === 0 ? counter = database.length - 1 : counter--
