2ee97553d6970f9aaeb08d3c689abb57ee2c9b86
Istituto Don Bosco <100870601+donboscopadova@users.noreply.github.com>
2022-04-10 12:09:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,20 +18,20 @@ const database = [
         "arguments": "guerra in Ucraina, interviste, Olimpiadi e molto altro!",
         "path": "/pdf/giornalino.pdf"
     },
-    {
-        "month": "Aprile",
-        "day": "10 Aprile 2022", 
-        "pages": "15", 
-        "arguments": "Pace nel mondo e altro...!",
-        "path": "/pdf/giornalino.pdf"
-    },
-    {
-        "month": "Giugno",
-        "day": "10 Giugno 2022", 
-        "pages": "12", 
-        "arguments": "Fine della guerra e sospensione del giornalino!",
-        "path": "/pdf/giornalino.pdf"
-    }
+    // {
+    //     "month": "Aprile",
+    //     "day": "10 Aprile 2022", 
+    //     "pages": "15", 
+    //     "arguments": "Pace nel mondo e altro...!",
+    //     "path": "/pdf/giornalino.pdf"
+    // },
+    // {
+    //     "month": "Giugno",
+    //     "day": "10 Giugno 2022", 
+    //     "pages": "12", 
+    //     "arguments": "Fine della guerra e sospensione del giornalino!",
+    //    "path": "/pdf/giornalino.pdf"
+    // }
 ]
 
 // Display datas
@@ -51,20 +51,20 @@ const start = (n) => {
 
 window.addEventListener('load', start(counter))
 
-leftArrow.addEventListener('click', () => {
-    counter === 0 ? counter = database.length - 1 : counter--
+// leftArrow.addEventListener('click', () => {
+//     counter === 0 ? counter = database.length - 1 : counter--
+// 
+//     start(counter)
+// })
 
-    start(counter)
-})
+// rightArrow.addEventListener('click', () => {
+//     counter === database.length - 1 ? counter = 0 : counter++
+//     
+//     start(counter)
+// })
 
-rightArrow.addEventListener('click', () => {
-    counter === database.length - 1 ? counter = 0 : counter++
-    
-    start(counter)
-})
-
-dots.forEach((dot, numb) => {
-    dot.addEventListener('click', () => {
-        start(numb)
-    })
-})
+// dots.forEach((dot, numb) => {
+//    dot.addEventListener('click', () => {
+//        start(numb)
+//    })
+// })
