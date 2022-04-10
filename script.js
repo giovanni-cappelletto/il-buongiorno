073151a20dbaf0221f62dc2d6e636147e7afe575
Istituto Don Bosco <100870601+donboscopadova@users.noreply.github.com>
2022-04-10 11:16:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const args = document.querySelector('#args')
 const periodical = document.querySelector('.periodical')
 const leftArrow = document.querySelector('#left-arrow')
 const rightArrow = document.querySelector('#right-arrow')
-//const dots = document.querySelectorAll('.dot')
-let counter = 0
+const dots = document.querySelectorAll('.dot')
+let counter = 0, mese
 
 // Database
 const database = [
@@ -16,6 +16,20 @@ const database = [
         "day": "10 Marzo 2022", 
         "pages": "20", 
         "arguments": "guerra in Ucraina, interviste, Olimpiadi e molto altro!",
+        "path": "/pdf/giornalino.pdf"
+    },
+    {
+        "month": "Aprile",
+        "day": "10 Aprile 2022", 
+        "pages": "15", 
+        "arguments": "Pace nel mondo e altro...!",
+        "path": "/pdf/giornalino.pdf"
+    },
+    {
+        "month": "Giugno",
+        "day": "10 Giugno 2022", 
+        "pages": "12", 
+        "arguments": "Fine della guerra e sospensione del giornalino!",
         "path": "/pdf/giornalino.pdf"
     }
 ]
@@ -28,34 +42,29 @@ const start = (n) => {
     args.textContent = database[n].arguments
     periodical.setAttribute('href', database[n].path) 
 
-    //for (let i = 0; i < dots.length; i++) {
-    //    dots[i].classList.remove('active')
-    //}
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('active')
+    }
 
-    //dots[n].classList.add('active')
+    dots[n].classList.add('active')
 }
 
 window.addEventListener('load', start(counter))
 
-leftArrow.disabled = true
-rightArrow.disabled = true
+leftArrow.addEventListener('click', () => {
+    counter === 0 ? counter = database.length - 1 : counter--
 
+    start(counter)
+})
 
+rightArrow.addEventListener('click', () => {
+    counter === database.length - 1 ? counter = 0 : counter++
+    
+    start(counter)
+})
 
-//leftArrow.addEventListener('click', () => {
-//    counter === 0 ? counter = database.length - 1 : counter--
-//
-//    start(counter)
-//})
-
-//rightArrow.addEventListener('click', () => {
-//    counter === database.length - 1 ? counter = 0 : counter++
-//    
-//    start(counter)
-//})
-
-//dots.forEach((dot, numb) => {
-//    dot.addEventListener('click', () => {
-//        start(numb)
-//    })
-//})
+dots.forEach((dot, numb) => {
+    dot.addEventListener('click', () => {
+        start(numb)
+    })
+})
